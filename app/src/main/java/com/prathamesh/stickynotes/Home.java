d13f21445f50df924e36cbe2b8eabf5065c2fadd
Prathamesh Adate <prathameshadate05@gmail.com>
2022-10-14 15:20:49 +0530

    Added New Note Acticity

--- app/src/main/java/com/prathamesh/stickynotes/Home.java
+++ app/src/main/java/com/prathamesh/stickynotes/Home.java
@@ -2,13 +2,25 @@ package com.prathamesh.stickynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Home extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fab = findViewById(R.id.FAB_CreateNote);
+
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this,NewNote.class);
+            startActivity(intent);
+        });
     }
 }
