90209b0548d5febb8dd999d15db9d8666d412e9f
Prathamesh Adate <prathameshadate05@gmail.com>
2022-10-14 19:45:08 +0530

    Added ShowNote & UpdateNote Activity

--- app/src/main/java/com/prathamesh/stickynotes/Home.java
+++ app/src/main/java/com/prathamesh/stickynotes/Home.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Home extends AppCompatActivity {
 
     FloatingActionButton fab;
+    Button show , update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,19 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         fab = findViewById(R.id.FAB_CreateNote);
+
+        show = findViewById(R.id.temp_show);
+        update = findViewById(R.id.temp_update);
+
+        show.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this,ShowNote.class);
+            startActivity(intent);
+        });
+
+        update.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this,UpdateNote.class);
+            startActivity(intent);
+        });
 
         fab.setOnClickListener(view -> {
             Intent intent = new Intent(Home.this,NewNote.class);
---
+++ app/src/main/java/com/prathamesh/stickynotes/ShowNote.java
@@ -0,0 +1,14 @@
+package com.prathamesh.stickynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_note);
+    }
+}
