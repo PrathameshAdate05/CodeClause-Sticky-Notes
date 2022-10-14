ff343eee3b4fd1c9b361fda64e27cb3fc749bfb5
Prathamesh Adate <prathameshadate05@gmail.com>
2022-10-14 10:07:11 +0530

    Home Activity Added

---
+++ app/src/main/java/com/prathamesh/stickynotes/Constants.java
@@ -0,0 +1,5 @@
+package com.prathamesh.stickynotes;
+
+public class Constants {
+    public static final int SPLASH = 3000;
+}
--- app/src/main/java/com/prathamesh/stickynotes/Splash.java
+++ app/src/main/java/com/prathamesh/stickynotes/Splash.java
@@ -2,7 +2,9 @@ package com.prathamesh.stickynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import java.util.Objects;
 
@@ -13,5 +15,14 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },Constants.SPLASH);
     }
 }
